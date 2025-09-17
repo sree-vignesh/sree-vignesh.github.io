3a1d24cd44b7112ab0a15af343f4dd4bb4513e17
sree-vignesh <sreevignesh256@gmail.com>
2025-09-17 15:42:39 +0530

    Navbar basic.

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,23 +1,18 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 import "./Navbar.css";
 
 function NavigationBar() {
   return (
-    <Navbar expand="lg" className="custom-navbar" fixed="top">
-      <Container>
-        <Navbar.Brand href="#home" className="brand">
-          BrandName
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="sidebar">
+      <Nav className="flex-column nav-links">
+      <Nav.Link href="#hero" className="vertical-text">           </Nav.Link>
+        <Nav.Link href="#about" className="vertical-text">About Me</Nav.Link>
+        <Nav.Link href="#projects" className="vertical-text">Projects</Nav.Link>
+        <Nav.Link href="#contact" className="vertical-text">Contact</Nav.Link>
+
+      </Nav>
+    </div>
   );
 }
 
