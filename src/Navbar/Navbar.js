911832f4af272025e9899843b7a89d1ffb706d4c
sree-vignesh <sreevignesh256@gmail.com>
2025-11-19 20:16:12 +0530

    Mobile Menu Fixed

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Nav } from "react-bootstrap";
 import "./Navbar.css";
 
 function NavigationBar() {
-  return (
-    <div className="sidebar">
-      <Nav className="flex-row nav-links">
-      {/* <Nav.Link href="#hero" className="vertical-text">           </Nav.Link> */}
-        <Nav.Link href="#about" className="vertical-text">About Me</Nav.Link>
-        <Nav.Link href="#projects" className="vertical-text">Projects</Nav.Link>
-        <Nav.Link href="#contact" className="vertical-text">Contact</Nav.Link>
+  const [open, setOpen] = useState(false);
 
-      </Nav>
-    </div>
+  return (
+    <>
+      {/* DESKTOP NAVBAR */}
+      <div className="sidebar">
+        <Nav className="nav-links">
+          <Nav.Link href="#about">About Me</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </div>
+
+      {/* MOBILE HAMBURGER */}
+      <div
+        className={`mobile-menu-top ${open ? "active" : ""}`}
+        onClick={() => setOpen(!open)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* MOBILE POPUP */}
+      <div className={`mobile-popup ${open ? "show" : ""}`}>
+        <a href="#about" onClick={() => setOpen(false)}>
+          About Me
+        </a>
+        <a href="#projects" onClick={() => setOpen(false)}>
+          Projects
+        </a>
+        <a href="#contact" onClick={() => setOpen(false)}>
+          Contact
+        </a>
+      </div>
+    </>
   );
 }
 
